918e924d445b65fa7102d6a6365c33bc920a8384
Luis Herranz <luisherranz@gmail.com>
2016-11-25 18:28:38 +0100

    Change Root to new format.

--- src/dashboard/components/Root/index.jsx
+++ src/dashboard/components/Root/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Publish from '../Publish';
+import * as deps from '../../deps';
 
-export default () => (
-  <Publish />
-);
+export default () => {
+  const RootContainer = deps.elements.RootContainer;
+  return (
+    <RootContainer>
+      <Publish />
+    </RootContainer>
+  );
+};
