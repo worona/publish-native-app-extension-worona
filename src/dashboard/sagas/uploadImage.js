168307e42c515b46258d89ea1284756661053ab8
Eduardo Campaña <asrielo@gmail.com>
2017-07-04 11:50:15 +0200

    Fixed upload url (this time for sure) and fixed content-type error.

--- src/dashboard/sagas/uploadImage.js
+++ src/dashboard/sagas/uploadImage.js
@@ -9,11 +9,11 @@ function* uploadImage(action) {
   try {
     const { siteId, file, fileId } = action;
     const fileName = `${fileId}_${file.name}`;
+    const backend = window.location.host.startsWith('dashboard') ? 'backend' : 'prebackend';  // eslint-disable-line
     const url = (yield request
-      .get('https://prebackend.worona.io/api/v1/s3/sign')
+      .get(`https://${backend}.worona.io/api/v1/s3/sign`)
       .query({ siteId, imgType: 'icon', objectName: fileName })).text;
-
-    yield request.put(url).send(file);
+    yield request.put(url).set('Content-Type', file.type).send(file);
 
     yield put(uploadSucceed(fileName, siteId));
   } catch (error) {
